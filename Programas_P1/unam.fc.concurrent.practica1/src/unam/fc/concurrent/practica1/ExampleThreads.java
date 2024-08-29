9731f0afe77726ce6e20b3f2e3feed6bd801469e
surindt <gilderojim@gmail.com>
2024-08-29 13:02:24 -0600

    descripcion de P2 y modificacion de P1

--- Programas_P1/unam.fc.concurrent.practica1/src/unam/fc/concurrent/practica1/ExampleThreads.java
+++ Programas_P1/unam.fc.concurrent.practica1/src/unam/fc/concurrent/practica1/ExampleThreads.java
@@ -1,5 +1,6 @@
 package unam.fc.concurrent.practica1;
-
+import unam.fc.concurrent.practica1.ThreadExtends;
+import unam.fc.concurrent.practica1.ThreadRunnable;
 //Programa 1: Creacion de un hilo
 //   Dos formas: Forma de creacion del Hilo A o forma de creacion del hilo B
 //   Utiliza las clases: ThreadExtends y ThreadRunnable
@@ -7,10 +8,12 @@ package unam.fc.concurrent.practica1;
 public class ExampleThreads{
 	public static void main(String[] args) {
 		//threadA es un hilo que extiende la clase Thread
+		ThreadExtends threadC = new ThreadExtends();
 		ThreadExtends threadA= new ThreadExtends(); 
 		//threadB es un hilo Thread al cual se le pasa una instancia de Runnable
 		Thread threadB= new Thread(new ThreadRunnable()); 
 		threadA.start();
 		threadB.start();
+		threadC.start();
 	}
 }
